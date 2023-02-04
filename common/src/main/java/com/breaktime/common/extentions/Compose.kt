97ba1886ce2888cabb7705de8437e1200add77b3
Elisey Budnitsky <bgs6544@gmail.com>
2023-02-04 17:37:47 +0300

    add common module

---
+++ common/src/main/java/com/breaktime/common/extentions/Compose.kt
@@ -0,0 +1,36 @@
+package com.breaktime.common.extentions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+
+@Preview(device = "id:Nexus One")
+@Preview(device = "id:Nexus S")
+@Preview(device = "id:Galaxy Nexus")
+@Preview(device = "id:Nexus 4")
+@Preview(device = "id:Nexus 5")
+@Preview(device = "id:pixel")
+@Preview(device = "id:pixel_2")
+@Preview(device = "id:Nexus 5X")
+@Preview(device = "id:pixel_3")
+@Preview(device = "id:pixel_xl")
+@Preview(device = "id:pixel_3a")
+@Preview(device = "id:Nexus 6P")
+@Preview(device = "id:pixel_4")
+@Preview(device = "id:pixel_4a")
+@Preview(device = "id:Nexus 6")
+@Preview(device = "id:pixel_2_xl")
+@Preview(device = "id:pixel_3a_xl")
+@Preview(device = "id:pixel_5")
+@Preview(device = "id:pixel_3_xl")
+@Preview(device = "id:pixel_4_xl")
+@Preview(device = "id:pixel_6")
+@Preview(device = "id:pixel_6_pro")
+annotation class AllPhoneScreensPreview
+
+@Composable
+fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
+
+@Composable
+fun Number.pxToDp() = with(LocalDensity.current) { this@pxToDp.toFloat().toDp() }
