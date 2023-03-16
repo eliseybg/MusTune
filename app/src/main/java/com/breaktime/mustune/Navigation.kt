3a4a392318cd5e0291cd552a317ce1964db3760e
Elisey Budnitsky <yelisei.budnitski@intexsoft.by>
2023-03-16 16:41:14 +0300

    add on back pressed listener for navigation

--- app/src/main/java/com/breaktime/mustune/Navigation.kt
+++ app/src/main/java/com/breaktime/mustune/Navigation.kt
@@ -1,12 +1,21 @@
 package com.breaktime.mustune
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.breaktime.mustune.api.LoginEntry
+import com.breaktime.mustune.common.Constants
 import com.breaktime.mustune.common.find
 import com.breaktime.mustune.di.LocalAppProvider
 import com.breaktime.mustune.main.api.MainEntry
@@ -26,18 +35,48 @@ fun Navigation() {
     val songScreen = destinations.find<SongEntry>()
     val searchSongsScreen = destinations.find<SearchSongsEntry>()
     val settingsScreen = destinations.find<SettingsEntry>()
-
-    Box(Modifier.fillMaxSize()) {
-        NavHost(navController, startDestination = loginScreen.destination()) {
-            with(loginScreen) { composable(navController, destinations) }
-            with(mainScreen) {
-                subNavigation(destinations, musicScreen.destination()) {
-                    with(musicScreen) { composable(navController, destinations) }
-                    with(settingsScreen) { composable(navController, destinations) }
-                }
+    BackPressHandler(navController)
+    NavHost(navController, startDestination = loginScreen.destination()) {
+        with(loginScreen) { composable(navController, destinations) }
+        with(mainScreen) {
+            subNavigation(destinations, musicScreen.destination()) {
+                with(musicScreen) { composable(navController, destinations) }
+                with(settingsScreen) { composable(navController, destinations) }
             }
-            with(searchSongsScreen) { composable(navController, destinations) }
-            with(songScreen) { composable(navController, destinations) }
         }
+        with(searchSongsScreen) { composable(navController, destinations) }
+        with(songScreen) { composable(navController, destinations) }
+    }
+}
+
+@Composable
+fun BackPressHandler(
+    navController: NavController,
+    backPressedDispatcher: OnBackPressedDispatcher? = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+) {
+    val backStack by navController.currentBackStackEntryAsState()
+    val context = LocalContext.current
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            var lastBackPress = -1L
+            var toast: Toast? = null
+
+            override fun handleOnBackPressed() {
+                toast?.cancel()
+                if (lastBackPress + Constants.BACK_PRESS_DEBOUNCE_MILLS > System.currentTimeMillis()) {
+                    (context as? Activity)?.finish()
+                } else {
+                    toast = Toast
+                        .makeText(context, "Press back again to exit", Toast.LENGTH_SHORT)
+                        .also { it.show() }
+                }
+                lastBackPress = System.currentTimeMillis()
+            }
+        }
+    }
+
+    if (backStack != null) DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher?.addCallback(backCallback)
+        onDispose { backCallback.remove() }
     }
 }
